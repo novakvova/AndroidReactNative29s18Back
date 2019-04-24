449f1e48024d220724a4173435c319ca916261ba
novakvova <novakvova@gmail.com>
2019-04-24 18:38:19 +0300

    Begin work app

---
+++ AndroidBack29s18/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AndroidBack29s18.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AndroidBack29s18.Controllers
+{
+    
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Post([FromBody] LoginViewModel model)
+        {
+            return Ok(new { semen="Дякую" });
+        }
+    }
+}
